1bfd5b524f9b3f623b8e3e7a2cfbd53216d7532a
ARPIT VASHISHT <arpitvashisht1@gmail.com>
2020-02-22 18:16:27 +0530

    Add files via upload

---
+++ LinkedList/LengthOfLLRecursion.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+public class LengthOfLLRecursion {
+    Node head;
+    public static void main(String [] args)
+    {
+        LengthOfLLRecursion ll = new LengthOfLLRecursion();
+        ll.push(1);
+        ll.push(4);
+        ll.push(1);
+        ll.push(12);
+        ll.push(1);
+        ll.push(20);
+        System.out.println("Length of LL is "+ll.getCount());
+
+
+    }
+    public void push(int data)
+    {
+        Node node=new Node(data);
+        node.next=head;
+        head=node;
+    }
+    public int getCount()
+    {
+        return countByRec(head);
+
+    }
+    public int countByRec(Node node)
+    {
+        if(node==null)
+            return 0;
+
+        return 1+countByRec(node.next);
+
+    }
+}
